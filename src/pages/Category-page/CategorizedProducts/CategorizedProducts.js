7d4031385847d3eb002e700124dd21d8382b2841
shaheerasarama <shaheerasarama@gmail.com>
2024-04-13 20:40:32 +0200

    gridLayout

--- src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
+++ src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
@@ -4,41 +4,67 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import theme from "../../../theme/Theme";
-import styles from "./CategorizedProductsStyles";
 import ProductCard from "../../../components/ProductCard/ProductCard";
+import { Grid } from "@mui/material";
 export default function CategorizedProducts() {
   return (
-    <Box sx={{...styles.Parentgrid,padding:'50px 0'}}>
-      <Box>
-      <Breadcrumbs sx={{...styles.centerText,textAlign:'center'}} aria-label="breadcrumb" separator="›">
-        <Link
-          underline="hover"
-          sx={{ color: theme.palette.primary.main, fontWeight: "bold" }}
-          href="/"
-          aria-current="page"
-        >
-          Home
-        </Link>
-        <Link
-          underline="hover"
-          color="inherit"
-          href="/material-ui/getting-started/installation/"
-        >
-          HandBag
-        </Link>
-      </Breadcrumbs>
-      <Typography variant="h6" sx={{color:theme.palette.primary.main,fontWeight:'bold',padding:'15px 0'}}>Handbags</Typography>
-      </Box>
-      <Box sx={{...styles.ProductsGrid}}>
-
-        {/* Loop over one Ahmad */}
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard rating={4}/>
-      </Box>
+    <Box sx={{ padding: "50px 0" }}>
+      <Grid container>
+        <Grid item md={12} lg={2}>
+          <Box>
+            <Breadcrumbs
+              sx={{ textAlign: "center" }}
+              aria-label="breadcrumb"
+              separator="›"
+            >
+              <Link
+                underline="hover"
+                sx={{ color: theme.palette.primary.main, fontWeight: "bold" }}
+                href="/"
+                aria-current="page"
+              >
+                Home
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/material-ui/getting-started/installation/"
+              >
+                HandBag
+              </Link>
+            </Breadcrumbs>
+            <Typography
+              variant="h6"
+              sx={{
+                color: theme.palette.primary.main,
+                fontWeight: "bold",
+                padding: "15px 0",
+              }}
+            >
+              Handbags
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item md={12} lg={9}>
+          <Box>
+            <Grid container spacing={2}>
+            {/* Loop over one Ahmad */}
+              <Grid item xs={6} md={4}>
+                <ProductCard />
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <ProductCard />
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <ProductCard />
+              </Grid>
+              <Grid item xs={6} md={4}>
+                <ProductCard />
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
