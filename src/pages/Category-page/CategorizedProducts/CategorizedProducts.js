8df9f5003e4792427e648fdebc055bd18d240a0b
shaheerasarama <shaheerasarama@gmail.com>
2024-04-17 15:07:20 +0200

    breadcrumbs

--- src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
+++ src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
@@ -6,53 +6,18 @@ import Box from "@mui/material/Box";
 import theme from "../../../theme/Theme";
 import ProductCard from "../../../components/ProductCard/ProductCard";
 import { Grid } from "@mui/material";
+import BreadCrumbs from "../../../components/BreadCrumbs/BreadCrumbs";
 export default function CategorizedProducts() {
   return (
     <Box sx={{ padding: "50px 0" }}>
       <Grid container>
         <Grid item sm={12} md={12} lg={2}>
-          <Box>
-            <Breadcrumbs
-              sx={{ textAlign: "center" }}
-              aria-label="breadcrumb"
-              separator="›"
-            >
-              <Link
-                underline="hover"
-                sx={{ color: theme.palette.primary.main, fontWeight: "bold" }}
-                href="/"
-                aria-current="page"
-              >
-                Home
-              </Link>
-              <Link
-                underline="hover"
-                color="inherit"
-                href="/material-ui/getting-started/installation/"
-              >
-                HandBag
-              </Link>
-            </Breadcrumbs>
-            <Typography
-              variant="h6"
-              sx={{
-                color: theme.palette.primary.main,
-                fontWeight: "bold",
-                padding: "15px 0",
-              }}
-            >
-              Handbags
-            </Typography>
-          </Box>
+          <BreadCrumbs/>
         </Grid>
         <Grid item sm={12} md={12} lg={10}>
-          <Box>
             <Grid container spacing={2}>
             {/* Loop over one Ahmad */}
               <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard rating={4}/>
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} lg={3}>
                 <ProductCard />
               </Grid>
               <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -61,8 +26,14 @@ export default function CategorizedProducts() {
               <Grid item xs={12} sm={6} md={4} lg={3}>
                 <ProductCard />
               </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <ProductCard />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <ProductCard />
+              </Grid>
+
             </Grid>
-          </Box>
         </Grid>
       </Grid>
     </Box>
