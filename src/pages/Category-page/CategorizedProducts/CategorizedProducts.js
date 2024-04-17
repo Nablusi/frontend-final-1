ca90f5eacc4073d922a6bb1c07486adbe7748e6c
YAQOT <63197328+YAQ0T@users.noreply.github.com>
2024-04-17 19:21:18 +0200

    remove warnings

--- src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
+++ src/pages/Category-page/CategorizedProducts/CategorizedProducts.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import theme from "../../../theme/Theme";
 import ProductCard from "../../../components/ProductCard/ProductCard";
 import { Grid } from "@mui/material";
 import BreadCrumbs from "../../../components/BreadCrumbs/BreadCrumbs";
@@ -12,28 +8,27 @@ export default function CategorizedProducts() {
     <Box sx={{ padding: "50px 0" }}>
       <Grid container>
         <Grid item sm={12} md={12} lg={2}>
-          <BreadCrumbs/>
+          <BreadCrumbs />
         </Grid>
         <Grid item sm={12} md={12} lg={10}>
-            <Grid container spacing={2}>
+          <Grid container spacing={2}>
             {/* Loop over one Ahmad */}
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <ProductCard />
-              </Grid>
-
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
             </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
