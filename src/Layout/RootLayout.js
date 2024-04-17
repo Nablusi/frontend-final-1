7e6107b4c5335a3c3732d90a5c4e3e0ba69b96df
YAQOT <63197328+YAQ0T@users.noreply.github.com>
2024-04-17 19:34:10 +0200

    finish axios

--- src/Layout/RootLayout.js
+++ src/Layout/RootLayout.js
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation/Navigation";
 import { NavBarProvider } from "../contexts/NavBarContext";
 import Footer from "../components/Footer/Footer";
-import useAxios from "../Hooks/useAxios";
 
 export default function RootLayout() {
   return (
