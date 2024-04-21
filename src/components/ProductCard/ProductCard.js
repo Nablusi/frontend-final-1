5467d89045898480cfae25c425ee1936182be2d2
shaheerasarama <shaheerasarama@gmail.com>
2024-04-21 20:25:44 +0300

    branchEdits

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,13 @@
-import {React} from "react";
+import { React } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import Box from "@mui/material/Box";
 import theme from "../../theme/Theme";
 import { Link } from "react-router-dom";
-
+import GeneratedStars from "../../services/utils/GeneratedStars";
 export default function ProductCard({ rating, product }) {
   const styles = {
     text: {
@@ -19,102 +18,93 @@ export default function ProductCard({ rating, product }) {
       width: "70%",
       [theme.breakpoints.down("md")]: {
         flexDirection: "column",
-        alignItems:'flex-start',
+        alignItems: "flex-start",
         width: "100%",
       },
-      [theme.breakpoints.between("md","lg")]: {
+      [theme.breakpoints.between("md", "lg")]: {
         width: "80%",
       },
     },
   };
   return (
     <>
-      <Link style={{ textDecoration:'none' }}>
-      <Card sx={{ maxWidth: 350, boxShadow: "none",margin:'auto' }}>
-        <CardMedia
-          sx={{ height: 280, borderRadius: "10px" }}
-          image={require(`../../images/PinkBag.png`)}
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="subtitle1"
-            component="h2"
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              fontSize: "18px",
-              fontWeight: "600",
-              fontFamily: "Inter",
-            }}
-          >
-            {product.name}
-            <FavoriteBorderOutlinedIcon />
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="h3">
-            Pink Bag
-          </Typography>
-
-          {rating ? (
+      <Link style={{ textDecoration: "none" }}>
+        <Card sx={{ maxWidth: 350, boxShadow: "none", margin: "auto" }}>
+          <CardMedia
+            sx={{ height: 280, borderRadius: "10px" }}
+            image={require(`../../images/PinkBag.png`)}
+            title="green iguana"
+          />
+          <CardContent>
             <Typography
+              gutterBottom
+              variant="subtitle1"
+              component="h2"
               sx={{
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-                paddingTop: "8px",
-                fontWeight: "500",
-                color: theme.palette.primary.main,
+                fontSize: "18px",
+                fontWeight: "600",
+                fontFamily: "Inter",
               }}
             >
-              <Rating
-                name="read-only"
-                value={rating}
-                sx={{ color: theme.palette.primary.star }}
-                readOnly
-              />
-              <p>43 Rating</p>
-            </Typography>
-          ) : (
-            ""
-          )}
-          <Box
-            sx={{
-              ...styles.text,
-            }}
-          >
-            {product.discount === null ? (<Typography
-              sx={{ color: theme.palette.primary.main }}
-              variant="subtitle1"
-              component="p"
-            >
-              ${product.price}
-            </Typography>) : (<> <Typography
-              sx={{ color: theme.palette.primary.main }}
-              variant="subtitle1"
-              component="p"
-            >
-              $39.49
+              {product.name}
+              <FavoriteBorderOutlinedIcon />
             </Typography>
             <Typography
-              sx={{ textDecoration: "line-through" }}
               variant="subtitle1"
-              component="p"
+              color="text.secondary"
+              component="h3"
             >
-              $78.66
+              Pink Bag
             </Typography>
-            <Typography
-              sx={{ color: theme.palette.primary.error }}
-              variant="subtitle1"
-              component="p"
+
+            {/* Here I will pass the review of the product but still don't have review data from the back-end team => product.reviews.rating + the count of them */}
+
+            {rating == true ? <GeneratedStars /> : ""}
+            <Box
+              sx={{
+                ...styles.text,
+              }}
             >
-              50%OFF
-            </Typography></>)}
-           
-          </Box>
-        </CardContent>
-      </Card>
+              {product.discount === null ? (
+                <Typography
+                  sx={{ color: theme.palette.primary.main }}
+                  variant="subtitle1"
+                  component="p"
+                >
+                  ${product.price}
+                </Typography>
+              ) : (
+                <>
+                  {" "}
+                  <Typography
+                    sx={{ color: theme.palette.primary.main }}
+                    variant="subtitle1"
+                    component="p"
+                  >
+                    $39.49
+                  </Typography>
+                  <Typography
+                    sx={{ textDecoration: "line-through" }}
+                    variant="subtitle1"
+                    component="p"
+                  >
+                    $78.66
+                  </Typography>
+                  <Typography
+                    sx={{ color: theme.palette.primary.error }}
+                    variant="subtitle1"
+                    component="p"
+                  >
+                    50%OFF
+                  </Typography>
+                </>
+              )}
+            </Box>
+          </CardContent>
+        </Card>
       </Link>
     </>
   );
