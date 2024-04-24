d3b428b530c026d6cccffe94244a9c6edec636e4
shaheerasarama <shaheerasarama@gmail.com>
2024-04-25 00:52:18 +0300

    add To Cart Token

--- src/services/utils/authUser.js
+++ src/services/utils/authUser.js
@@ -2,10 +2,8 @@ export const authUser = () => {
     try {
         const token = localStorage.getItem('token') ||  sessionStorage.getItem('token') || '';
         if (token !== '') {
-            console.log('Token exists:', token);
             return true;
         } else {
-            console.log('Token does not exist or is invalid:', token);
             return false;
         }
     } catch (error) {
