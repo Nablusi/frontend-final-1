4eec7521c708654d64a211edc8931e3cbbfcb827
nablusi <zaidsaeh3@gmail.com>
2024-04-25 10:45:30 +0300

    adding check function for cart

--- src/services/utils/emailAuth.js
+++ src/services/utils/emailAuth.js
@@ -2,10 +2,8 @@ export const emailAuth = () => {
     try {
         const email = localStorage.getItem('email') || sessionStorage.getItem('email') || '';
         if (email !== '') {
-            console.log('email exists:', email);
             return email;
         } else {
-            console.log('email does not exist or is invalid:', email);
             return "z";
         }
     } catch (error) {
