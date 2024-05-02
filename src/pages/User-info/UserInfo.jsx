6990a47618fd63c6f30a13ff7bbb995532c82023
nablusi <zaidsaeh3@gmail.com>
2024-05-02 11:43:02 +0300

    i dont know

--- src/pages/User-info/UserInfo.jsx
+++ src/pages/User-info/UserInfo.jsx
@@ -1,25 +1,16 @@
-import React, { useState , useEffect } from "react";
-import { useForm } from "react-hook-form";
-
+import React, { useState } from "react";
 import UserInfoSideBar from "../../components/User-Info-SideBar/UserInfoSideBar";
 import { Box, Container } from "@mui/system";
 import TitleUserInformation from "./Title-User-information/TitleUserInformation";
+
+import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import useAxios from "../../services/Hooks/useAxios";
 import { Outlet } from "react-router-dom";
 
-import { MyOrders } from "./MyOrders/MyOrders";
-import axios from "axios";
-
-
-
-
 export default function UserInfo() {
   const [selectedOrderTab, setSelectedOrderTab] = useState(0);
   const [active, setActive] = useState("personalInformation");
-    const [selectedTab, setSelectedTab] = useState(0);
-  const [filteredOrders, setFilteredOrders] = useState([]);
-
   const {
     register,
     handleSubmit,
@@ -45,49 +36,6 @@ export default function UserInfo() {
   const handleActive = (data) => {
     setActive(data);
   };
-  const handleChanges = (event, newValue) => {
-    setSelectedTab(newValue);
-  }
-
-
-  
-  
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const response = await axios.get(
-          "https://backend-final-1-latest.onrender.com/api/orders/user/20",
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwLCJpYXQiOjE3MTQ1NzkzOTV9.3bERkDj1cZjYFWzH_h57tF9Lno5dYCq3a8IGrjuoFIo",
-            },
-          }
-        );
-        console.log(response.data);
-        setFilteredOrders(response.data);
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-      }
-    };
-
-    fetchOrders();
-  }, []);
-
-  useEffect(() => {
-    console.log('useEffect triggered with selectedTab:', selectedTab);
-    let filtered = [];
-    if (filteredOrders && filteredOrders.length > 0) {
-      if (selectedTab === 0) {
-        filtered = filteredOrders.filter((order) => order.status === "approved");
-      }  else if (selectedTab === 2) {
-        filtered = filteredOrders.filter((order) => order.status === "done");
-      }
-    }
-    setFilteredOrders(filtered);
-    
-  }, [selectedTab]);
-
 
   const handleChange = (event, newValue) => {
     setSelectedOrderTab(newValue);
